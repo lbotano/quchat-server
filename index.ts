7f0f8b259dd781015454caff8a703a110039ac2e
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-12-23 00:31:38 -0300

    First working version

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
 import socketio, { Server } from 'socket.io';
 import jwt from 'jsonwebtoken';
 
+dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+type MessageRequest = {
+  token: string,
+  message: string
+};
 
 app.get('/', (_req : express.Request, res : express.Response) => {
   res.sendFile(__dirname + '/index.html');
@@ -13,26 +20,30 @@ app.get('/', (_req : express.Request, res : express.Response) => {
 
 io.on('connection', (socket : socketio.Socket) => {
   console.log('a user has connected');
+
   socket.on('authenticate', (msg : string) => {
     const token = jwt.sign(msg, 'shh');
     console.log(`${msg} logged in.`);
     socket.emit('authenticate', token);
   });
-  socket.on('chat message', (msg : string) => {
-    const token = socket.handshake.headers.authorization;
-    if (token && token.substring(0, 7).toLowerCase() === 'bearer ') {
-      try {
-        const decodedToken = jwt.verify(token.substring(7), 'shh');
-        console.log(`${decodedToken}: ${msg}`);
-        io.emit('chat message', `${decodedToken}: ${msg}`);
-      } catch (error) {
-        console.error(error);
-        socket.emit('error', 'authentication error');
-      }
+
+  socket.on('chat message', (msg : MessageRequest) => {
+    const token = msg.token;
+
+    try {
+      const decodedToken = jwt.verify(token, 'shh');
+      console.log(`${decodedToken}: ${msg.message}`);
+      io.emit('chat message', {
+        username: decodedToken,
+        message: msg.message
+      });
+    } catch (error) {
+      console.error(error);
+      socket.emit('error', 'authentication error');
     }
   });
 });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+server.listen(process.env.PORT, () => {
+  console.log(`listening on *:${process.env.PORT}`);
 });
